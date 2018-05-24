6a609f3ada76b8a20b13532341d7ccb65b455b7e
kiranjajal <37736509+kiranjajal@users.noreply.github.com>
2018-05-24 15:55:03 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,40 @@
+import sys
+import p1
+
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
+
+try:
+    import ttk
+
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+
+    py3 = True
+
+
+def init(top, gui, *args, **kwargs):
+    global w, top_level, root
+    w = gui
+    top_level = top
+    root = top
+
+
+def destroy_window():
+    # Function which closes the window.
+    global top_level
+    top_level.destroy()
+    top_level = None
+
+
+def New_Toplevel():
+    New_Toplevel()
+
+
+if __name__ == '__main__':
+    import page1
+
+    page1.vp_start_gui()
